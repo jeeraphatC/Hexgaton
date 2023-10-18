e42f1592ef6ecb818ea0a3962e63acb28e4a6865
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-10-18 23:40:57 +0700

    fix pom

--- DesignDevTunes/freelance-service/src/main/java/freelance/service/freelanceservice/Freelance.java
+++ DesignDevTunes/freelance-service/src/main/java/freelance/service/freelanceservice/Freelance.java
@@ -1,6 +1,8 @@
 package freelance.service.freelanceservice;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
--- DesignDevTunes/freelance-service/src/main/java/freelance/service/freelanceservice/FreelanceRepository.java
+++ DesignDevTunes/freelance-service/src/main/java/freelance/service/freelanceservice/FreelanceRepository.java
@@ -1,5 +1,7 @@
 package freelance.service.freelanceservice;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FreelanceRepository extends JpaRepository<Freelance, String> {
