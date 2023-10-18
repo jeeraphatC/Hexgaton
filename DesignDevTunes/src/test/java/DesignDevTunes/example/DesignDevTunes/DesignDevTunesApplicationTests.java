41cf01c353ac08367893b2c0519ae3d9badffdf6
Siwasith <118959965+Siwasith@users.noreply.github.com>
2023-10-18 22:04:43 +0700

    add base project

---
+++ DesignDevTunes/src/test/java/DesignDevTunes/example/DesignDevTunes/DesignDevTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package DesignDevTunes.example.DesignDevTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignDevTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
