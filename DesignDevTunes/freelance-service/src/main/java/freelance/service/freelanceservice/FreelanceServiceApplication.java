1d8ae142655e3869ba3b1f4688b06b1042531888
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-10-18 23:17:31 +0700

    freelance && enterprise

---
+++ DesignDevTunes/freelance-service/src/main/java/freelance/service/freelanceservice/FreelanceServiceApplication.java
@@ -0,0 +1,13 @@
+package freelance.service.freelanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceServiceApplication.class, args);
+	}
+
+}
