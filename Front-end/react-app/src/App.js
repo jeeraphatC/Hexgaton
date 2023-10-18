07cc4f61376fdcb4a483645286f7a1d05eed0287
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-10-18 21:47:10 +0700

    update-font

--- Front-end/react-app/src/App.js
+++ Front-end/react-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Navbar from './features/Navbar';
+import Home from './features/Home';
+import GlobalStyle from './features/GlobalStyle';
+import Container from './features/Container';
+import { Route, Routes , useLocation } from 'react-router-dom'; // Removed 'Switch'
+import Login from './features/Login';
+import Develop from './features/features-navbar/develop';
+import Register from './features/Regis';
 function App() {
+  const location = useLocation();
+
+  // Determine whether to show the Navbar based on the current route
+  const showNavbar = location.pathname !== '/';
+  const showNavbar1 = location.pathname !== '/register';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      { showNavbar1 && showNavbar && <Navbar />}
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/develop" element={<Develop />} />
+          </Routes>
+        </Container>
+      
+    </>
   );
 }
 
