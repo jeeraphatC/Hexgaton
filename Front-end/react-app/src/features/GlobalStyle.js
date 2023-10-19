370c40490e69893ab2390502220e3eb6be3e8a20
Siwasith <118959965+Siwasith@users.noreply.github.com>
2023-10-20 04:16:25 +0700

    add some css

--- Front-end/react-app/src/features/GlobalStyle.js
+++ Front-end/react-app/src/features/GlobalStyle.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
@@ -13,12 +13,17 @@ const GlobalStyle = createGlobalStyle`
     font-weight: normal;
     font-style: normal;
   }
+ 
   
   body {
     margin: 0;
     font-family: 'Bebas Neue', sans-serif;
     
+      text-decoration: none; /* ลบเส้นใต้เฉพาะเมื่อชี้ลงไปที่ลิงค์ */
+  
   }
+
+  
 
     * {
   box-sizing: border-box;
@@ -50,6 +55,7 @@ const GlobalStyle = createGlobalStyle`
     border-radius: 0.25rem;
 
     width: 100%;
+    
   }
 
   form button {
@@ -76,10 +82,119 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  form .input-group {
-    margin-bottom: 1.5rem;
+  
+  
+  .app-container {
+    max-width: auto;
+    position: absolute;
+  }
+  
+  .min-logo {
+    max-width:150px;
+    position: absolute;
+  }
+  .containermt-4{
+    display: flex;
+    justify-content: center;
+    padding:100px;
+  }
+  .card{
+    width: 600px;
+    border:0px;
+    box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.2);
+  }
+  .big-logo {
+    position: relative;
+    width: 300px;
+    
+  }
+  .biglogo-container{
+    display: flex;
+    justify-content: center;
+    margin:50px;
+  }
+
+  .form-text{
+   
+  }
+
+  .form-control{
+    border:0px;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    padding:20px;
+    height: 60px;
+    margin-top:30px;
+  }
+  .form-group{
+    margin-left:40px;
+    margin-right:40px;
+    margin-top:30px;
+  }
+  .alogin{
+    margin-left:50px;
+    font-size:20px;
+    font-weight: bold;
+    position:absolute;
+  }
+  .form-container{
+    
+  }
+  .asignup{
+    color: #9C9C9C;
+    margin-left:480px;
+    font-size:20px;
+    font-weight: bold;
+  }
+  .svg-container {
+    
+}
+.svg-container{
+    
+}
+  svg{
+  position:absolute;
+  }
+  .svg2{
+    margin-top: 200px;
+  }
+  .svg3{
+    margin-top: 400px;
+    margin-left: 20px;
+  }
+  .svg4{
+    margin-top: 450px;
+    margin-left: 1200px;
+  }
+  .svg5{
+    margin-top: 300px;
+    margin-left: 1830px;
+  }
+  .svg6{
+    margin-top: 550px;
+    margin-left: 400px;
+  }
+  .svg7{
+    margin-top: 0px;
+    margin-left: 1200px;
+  }
+
+  .btn btn-primary mt-4{
+    display: block;
+     margin: 0 auto;
+     
+  }
+  .btnrge{
+    display: flex;
+    justify-content: center;
+    margin-bottom: 40px;
   }
     
+  .btn-large {
+    width: 250px; /* กำหนดความกว้าง */
+    height: 45px; /* กำหนดความสูง */
+    font-size: 20px; /* กำหนดขนาดตัวอักษร */
+  }
+  
   `;
 
 export default GlobalStyle;
--- Front-end/react-app/src/features/Regis.js
+++ Front-end/react-app/src/features/Regis.js
@@ -1,137 +1,246 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import PropTypes from "prop-types";
+import styled from "styled-components";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import mlogo from "./pic/mini_logo.png";
+import blogo from "./pic/big_logo.png";
 
 function Register({ className }) {
-    const [accountname, setAccountname] = useState("");
-    const [email, setEmail] = useState("");
-    const [numberCard, setnumberCard] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const navigate = useNavigate();
-    const namePattern = /^[A-Za-z\s]+$/; // Allows only letters and spaces
-    const emailPattern = /.*@gmail\.com$/;
-    const passwordPattern = /^.{6,}$/;
-    const numberCardPattern = /^\d{13}$/;
-    const validateForm = () => {
-        return accountname.length > 0 && email.length > 0 && numberCard.length > 0 && password.length > 0;
-    };
+  const [accountname, setAccountname] = useState("");
+  const [email, setEmail] = useState("");
+  const [numberCard, setnumberCard] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const namePattern = /^[A-Za-z\s]+$/; // Allows only letters and spaces
+  const emailPattern = /.*@gmail\.com$/;
+  const passwordPattern = /^.{6,}$/;
+  const numberCardPattern = /^\d{13}$/;
+  const validateForm = () => {
+    return (
+      accountname.length > 0 &&
+      email.length > 0 &&
+      numberCard.length > 0 &&
+      password.length > 0
+    );
+  };
 
-    const validateForm2 = () => {
-        const validName = namePattern.test(accountname);
-        const validEmail = emailPattern.test(email);
-        const validPassword = passwordPattern.test(password);
-        const validnumbercard = numberCardPattern.test(numberCard);
+  const validateForm2 = () => {
+    const validName = namePattern.test(accountname);
+    const validEmail = emailPattern.test(email);
+    const validPassword = passwordPattern.test(password);
+    const validnumbercard = numberCardPattern.test(numberCard);
 
-        return validName && validEmail && validnumbercard && validPassword;
-    };
+    return validName && validEmail && validnumbercard && validPassword;
+  };
 
-    async function save(event) {
-        event.preventDefault();
+  async function save(event) {
+    event.preventDefault();
 
-        const isFormValid = validateForm(); // Define isFormValid here
+    const isFormValid = validateForm(); // Define isFormValid here
 
-        if (!isFormValid) {
-            setErrorMessage("Please fill out all fields.");
-            return;
-        }
-        const isFormValid2 = validateForm2(); // Define isFormValid2 here
+    if (!isFormValid) {
+      setErrorMessage("Please fill out all fields.");
+      return;
+    }
+    const isFormValid2 = validateForm2(); // Define isFormValid2 here
 
-        if (!isFormValid2) {
-            setErrorMessage("Please fill out all fields correctly.");
-            return;
-        }
-
-        try {
-            await axios.post("http://localhost:8085/api/v1/accounts/save", {
-                accountname: accountname,
-                email: email,
-                numberCard: numberCard,
-                password: password,
-                
-            });
-
-            alert("Account Registration Successful");
-            navigate("/");
-        } catch (err) {
-            if (err.response && err.response.status === 400) {
-                setErrorMessage(err.response.data);
-            } else {
-                console.error(err);
-            }
-        }
+    if (!isFormValid2) {
+      setErrorMessage("Please fill out all fields correctly.");
+      return;
     }
 
-    return (
-        <div>
-            <div class="container mt-4" >
-                <div class="card">
-                    <h1>Student Registation</h1>
-                    {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-                    <form>
-                        <div class="form-group">
-                            <label>Employee name</label>
-                            <input type="text" class="form-control" id="employeename" placeholder="Enter Name"
-                                value={accountname}
-                                onChange={(event) => {
-                                    setAccountname(event.target.value);
-                                }}
-                                required
-                            />
-                        </div>
-                        <div class="form-group">
-                            <label>email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter Email"
+    try {
+      await axios.post("http://localhost:8085/api/v1/accounts/save", {
+        accountname: accountname,
+        email: email,
+        numberCard: numberCard,
+        password: password,
+      });
 
-                                value={email}
-                                onChange={(event) => {
-                                    setEmail(event.target.value);
-                                }}
-                                required
-                            />
+      alert("Account Registration Successful");
+      navigate("/");
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setErrorMessage(err.response.data);
+      } else {
+        console.error(err);
+      }
+    }
+  }
 
-                        </div>
+  return (
+    <div>
+      <div class="svg-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1100"
+          height="150"
+          viewBox="0 0 156 150"
+          fill="none"
+          class="svg2"
+        >
+          <ellipse cx="78.0146" cy="75" rx="77.5444" ry="75" fill="#0196FC" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="132"
+          height="127"
+          viewBox="0 0 132 127"
+          fill="none"
+          class="svg3"
+        >
+          <ellipse
+            cx="65.6543"
+            cy="63.5"
+            rx="65.6543"
+            ry="63.5"
+            fill="#0196FC"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="156"
+          height="150"
+          viewBox="0 0 156 150"
+          fill="none"
+          class="svg4"
+        >
+          <ellipse cx="78.186" cy="75" rx="77.5444" ry="75" fill="#0196FC" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="90"
+          height="104"
+          viewBox="0 0 90 104"
+          fill="none"
+          class="svg5"
+        >
+          <ellipse cx="54.2358" cy="52" rx="53.7641" ry="52" fill="#0196FC" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="290.533px"
+          height="400"
+          viewBox="0 0 156 150"
+          fill="none"
+          class="svg6"
+        >
+          <ellipse cx="78.186" cy="75" rx="77.5444" ry="75" fill="#0196FC" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="132"
+          height="127"
+          viewBox="0 0 132 127"
+          fill="none"
+          class="svg7"
+        >
+          <ellipse
+            cx="65.6543"
+            cy="63.5"
+            rx="65.6543"
+            ry="63.5"
+            fill="#0196FC"
+          />
+        </svg>
+      </div>
 
-                        <div class="form-group">
-                            <label>numberCard</label>
-                            <input type="text" class="form-control" id="idcard" placeholder="Enter IDCard"
+      <div className="app-container">
+        <img src={mlogo} alt="โลโก้" className="min-logo" />
+      </div>
+      <div class="containermt-4">
+        <div class="card">
+          <div className="biglogo-container">
+            <img src={blogo} alt="โลโก้" className="big-logo" />
+          </div>
+          {errorMessage && (
+            <div className="alert alert-danger">{errorMessage}</div>
+          )}
+          <Link to="/">
+            <a className="alogin">Login</a>
+          </Link>
+          <div className="asignup">Signup</div>
+          <div className="form-container">
+            <form>
+              <div class="form-group">
+                {/* <label className="form-text">Employee name</label> */}
+                <input
+                  type="text"
+                  class="form-control"
+                  id="employeename"
+                  placeholder="Name"
+                  value={accountname}
+                  onChange={(event) => {
+                    setAccountname(event.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div class="form-group">
+                {/* <label className="form-text">email</label> */}
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                  required
+                />
+              </div>
 
-                                value={numberCard}
-                                onChange={(event) => {
-                                    setnumberCard(event.target.value);
-                                }}
-                                required
-                            />
+              <div class="form-group">
+                {/* <label className="form-text">numberCard</label> */}
+                <input
+                  type="text"
+                  class="form-control"
+                  id="idcard"
+                  placeholder="IDCard"
+                  value={numberCard}
+                  onChange={(event) => {
+                    setnumberCard(event.target.value);
+                  }}
+                  required
+                />
+              </div>
 
-                        </div>
-
-                        <div class="form-group">
-                            <label>password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Enter password"
-
-                                value={password}
-                                onChange={(event) => {
-                                    setPassword(event.target.value);
-                                }}
-                                required
-                            />
-                        </div>
-                        <button type="submit" class="btn btn-primary mt-4" onClick={save}  >Save</button>
-                        <Link to="/">
-                            <button type="submit" >Login</button>
-                        </Link>
-
-                    </form>
-                </div>
-            </div>
+              <div class="form-group">
+                {/* <label className="form-text">password</label> */}
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div class="btnrge">
+                <buttonrge
+                  type="submit"
+                  class="btn btn-primary mt-4 btn-large"
+                  onClick={save}
+                >
+                  Register
+                </buttonrge>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 Register.propTypes = {
-    className: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
 };
 
 export default styled(Register)`
