24f0ac1995cc9de08e21fe85b07e14a97a2f6eeb
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-10-19 21:19:47 +0700

    back-end

--- Back-end/DesignDevTunes/RegisandLogin/src/main/java/hom/login/RegisandLogin/RegisandLoginApplication.java
+++ Back-end/DesignDevTunes/RegisandLogin/src/main/java/hom/login/RegisandLogin/RegisandLoginApplication.java
@@ -1,7 +1,10 @@
 package hom.login.RegisandLogin;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
