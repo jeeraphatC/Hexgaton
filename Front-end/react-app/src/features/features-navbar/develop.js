f22f62dfccdaeff2a81fc7385ed49899f868c785
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-10-22 21:16:24 +0700

    bootstrap

--- Front-end/react-app/src/features/features-navbar/develop.js
+++ Front-end/react-app/src/features/features-navbar/develop.js
@@ -1,23 +1,78 @@
-import React from 'react';
+
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-function Develop({ className }) {
+import React, { useState } from "react";
+import { Button, Card, Container, Row, Col } from 'react-bootstrap'
+const Counter = ({ className }) => {
   return (
     <div className={className}>
-      <h1>Welcome</h1>
-      <p>
-        Welcome to development 
-        To find job
-      </p>
+      <div >
+        <Container style={{ padding: 20, marginTop: 20 }}>
+          <br />
+          <h3 style={{ textAlign: 'center' }}>skfhkjdshfkjsdhkfhsdkhfkjdshfkjdshfkjhskfhskfhskhfdkjhfkjs</h3>
+          <br />
+          <Row>
+          <Col md={4}>
+              <Card style={{ padding: 20, width: 400}}>
+                <Card.Body>
+                  <Card.Img variant="top" src='https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D' ></Card.Img>
+                  <br />
+                  <Card.Title>Hello</Card.Title>
+                  <Card.Text>OKAY HELOO</Card.Text>
+                  <Card.Link href='www.google.com' target='_blank'>
+                    <Button variant="warning">
+                      Read
+                    </Button>
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card style={{ padding: 20, width: 400}}>
+                <Card.Body>
+                  <Card.Img variant="top" src='https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D' ></Card.Img>
+                  <br />
+                  <Card.Title>Hello</Card.Title>
+                  <Card.Text>OKAY HELOO</Card.Text>
+                  <Card.Link href='www.google.com' target='_blank'>
+                    <Button variant="warning">
+                      Read
+                    </Button>
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card style={{ padding: 20, width: 400}}>
+                <Card.Body>
+                  <Card.Img variant="top" src='https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D' ></Card.Img>
+                  <br />
+                  <Card.Title>Hello</Card.Title>
+                  <Card.Text>OKAY HELOO</Card.Text>
+                  <Card.Link href='www.google.com' target='_blank'>
+                    <Button variant="warning">
+                      Read
+                    </Button>
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
-}
+};
 
-Develop.propTypes = {
+
+
+Counter.propTypes = {
   className: PropTypes.string.isRequired
 };
 
-export default styled(Develop)`
-  /* Add your styling here */
+export default styled(Counter)`
+width: 100%;
+  margin-top: 50px;
+  background-color: azure;
 `;
