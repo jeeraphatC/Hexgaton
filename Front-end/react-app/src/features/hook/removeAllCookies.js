6e04a27dccf865d1c645098f0555a256f1adb8e1
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-11-02 23:29:57 +0700

    remove  all cookies

--- Front-end/react-app/src/features/hook/removeAllCookies.js
+++ Front-end/react-app/src/features/hook/removeAllCookies.js
@@ -1,14 +1,30 @@
 import Cookies from 'js-cookie';
 
-function removeAllCookies() {
-    const cookies = document.cookie.split(";");
-  
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i];
-      const eqPos = cookie.indexOf("=");
-      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name ;
-    }
+const removeAllCookies = () => {
+  // Get an array of all the cookie names
+  const allCookieNames = Object.keys(Cookies.get());
+
+  // Loop through the cookie names and remove them
+  for (let i = 0; i < allCookieNames.length; i++) {
+    Cookies.remove(allCookieNames[i]);
   }
+};
+
+export default removeAllCookies;
+
+
+
+// import Cookies from 'js-cookie';
+
+// function removeAllCookies() {
+//     const cookies = document.cookie.split(";");
   
-  export default removeAllCookies ;
+//     for (let i = 0; i < cookies.length; i++) {
+//       const cookie = cookies[i];
+//       const eqPos = cookie.indexOf("=");
+//       const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+//       document.cookie = name ;
+//     }
+//   }
+  
+//   export default removeAllCookies ;
