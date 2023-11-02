30cc467c0771fc0e000a00ac2b7fa6b77f81e6e0
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-11-02 23:20:43 +0700

    Navbar

---
+++ Front-end/react-app/src/features/hook/removeAllCookies.js
@@ -0,0 +1,14 @@
+import Cookies from 'js-cookie';
+
+function removeAllCookies() {
+    const cookies = document.cookie.split(";");
+  
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name ;
+    }
+  }
+  
+  export default removeAllCookies ;
