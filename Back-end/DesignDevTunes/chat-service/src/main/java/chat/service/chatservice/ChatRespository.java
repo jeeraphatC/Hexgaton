b3e3c417bc0f62cda349c803214f78bfdde7f0d0
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-11-06 02:25:53 +0700

    chat

--- Back-end/DesignDevTunes/chat-service/src/main/java/chat/service/chatservice/ChatRespository.java
+++ Back-end/DesignDevTunes/chat-service/src/main/java/chat/service/chatservice/ChatRespository.java
@@ -1,17 +1,17 @@
-// package chat.service.chatservice;
+package chat.service.chatservice;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// import chat.service.chatservice.model.ChatEntity;
+import chat.service.chatservice.model.ChatEntity;
 
 
-// @Repository
-// public interface ChatRespository extends JpaRepository<ChatEntity, Long>{
-//     List<ChatEntity> findBySenderNameAndReceiverNameOrderByTimestampAsc(
-//         String senderName, String receiverName);
+@Repository
+public interface ChatRespository extends JpaRepository<ChatEntity, Long>{
+    List<ChatEntity> findBySenderNameAndReceiverNameOrderByTimestampAsc(
+        String senderName, String receiverName);
 
-//     List<ChatEntity> findByReceiverNameOrderByTimestampAsc(String receiverName);
-// }
+    List<ChatEntity> findByReceiverNameOrderByTimestampAsc(String receiverName);
+}
