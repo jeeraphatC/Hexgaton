d886afe14e2c27da9dfdc7f122cc810e6df38d0b
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-11-06 02:20:10 +0700

    profile

--- Back-end/DesignDevTunes/chat-service/src/main/java/chat/service/chatservice/model/Status.java
+++ Back-end/DesignDevTunes/chat-service/src/main/java/chat/service/chatservice/model/Status.java
@@ -1,7 +1,7 @@
-package chat.service.chatservice.model;
+// package chat.service.chatservice.model;
 
-public enum Status {
-    JOIN,
-    MESSAGE,
-    LEAVE
-}
+// public enum Status {
+//     JOIN,
+//     MESSAGE,
+//     LEAVE
+// }
--- Front-end/react-app/src/features/Enterprises/Eprofile.js
+++ Front-end/react-app/src/features/Enterprises/Eprofile.js
@@ -11,7 +11,7 @@ import { Container, Row, Col, Card, Button, CardBody } from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
 
 
-function Fprofile({ className }) {
+function Eprofile({ className }) {
   const [workData, setWorkData] = useState([]);
   const [id, setId] = useState();
   const [cookies, setCookie, removeCookie] = useCookies();
@@ -93,7 +93,7 @@ function Fprofile({ className }) {
     const idFromCookies = getCookies("id");
   
     axios
-      .get(`http://localhost:8082/historys/enterprise`)
+      .get(`http://localhost:8082/historys/freelance`)
       
       .then((response) => {
         if (Array.isArray(response.data)) {
@@ -252,11 +252,11 @@ function Fprofile({ className }) {
   );
 }
 
-Fprofile.propTypes = {
+Eprofile.propTypes = {
   className: PropTypes.string.isRequired,
 };
 
-export default styled(Fprofile)`
+export default styled(Eprofile)`
 
 .edit .link-edit{
   color:#9c9c9c;
